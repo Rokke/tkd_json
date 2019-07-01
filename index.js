2e2a9b72b94657c5a68b861a044da6a911e270d6
Rokke <ragnartalgoe@gmail.com>
2019-07-01 20:15:59 +0200

    First version

---
+++ index.js
@@ -0,0 +1,17 @@
+var express=require('express')
+const fs=require('fs')
+
+var app=express()
+
+app.get('/',function (req,res) {
+  let cont=fs.readFileSync('/tmp/tkd_json/30964327.json')
+  if(cont){
+    let cjson=JSON.parse(cont)
+    console.log('file', cjson)
+    res.json(cjson)
+  }else res.sendStatus(500)
+})
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log('Lytter til port 3000!')
+})
