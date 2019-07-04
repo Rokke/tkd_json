bb9515c5c262aaf33af6e8810b47d5a48a433ed9
Rokke <ragnartalgoe@gmail.com>
2019-07-05 00:11:35 +0200

    Added tournament

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ app.get('/fights',function (req,res) {
   let cont=fs.readFileSync('/var/opt/tkd_json/30964327.json')
   if(cont){
     let cjson=JSON.parse(cont)
-    console.log('file', cjson)
+    console.log(req.originalUrl,cjson.fights,cjson.date)
+    res.json(cjson)
+  }else res.sendStatus(500)
+})
+app.get('/tournaments',function (req,res) {
+  let cont=fs.readFileSync('/var/opt/tkd_json/tournaments.json')
+  if(cont){
+    let cjson=JSON.parse(cont)
+    console.log(req.originalUrl,cjson.tournaments,cjson.date)
     res.json(cjson)
   }else res.sendStatus(500)
 })
