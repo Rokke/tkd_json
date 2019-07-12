74700de1438d80862e9582221f08bb92711c07b9
Rokke <ragnartalgoe@gmail.com>
2019-07-12 08:37:30 +0200

    Added put

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs=require('fs')
 var app=express()
 
 app.get('/fights/:tpss/:date',function (req,res) {
-	console.log("[%s] TPSS fights: %j",req.originalUrl,req.params)
+	console.log("[%s] TPSS fights: %j, %j",req.originalUrl,req.params,req.body)
 	let filename='/var/opt/tkd_json/'+req.params.tpss+'_'+req.params.date+'.json'
   let cont=fs.readFileSync(filename)
   if(cont){
@@ -13,13 +13,28 @@ app.get('/fights/:tpss/:date',function (req,res) {
     res.json(cjson)
   }else res.sendStatus(500)
 })
+app.put('/fights',function (req,res) {
+	console.log("[%s] TPSS fights put: %j, %j",req.originalUrl,req.params,req.body)
+	let filename='/var/opt/tkd_json/'+req.body.tpss+'_'+req.body.date+'.json'
+  let cont=fs.readFileSync(filename)
+  if(cont){
+    let cjson=JSON.parse(cont)
+    console.log(req.originalUrl,cjson.fights.length,cjson.date)
+    res.json(cjson)
+  }else res.sendStatus(500)
+})
 app.get('/tournaments',function (req,res) {
+	console.log("[%s] TPSS fights: %j",req.originalUrl,req.params)
   let cont=fs.readFileSync('/var/opt/tkd_json/tournaments.json')
   if(cont){
     let cjson=JSON.parse(cont)
     console.log(req.originalUrl,cjson.tournaments,cjson.date)
     res.json(cjson)
   }else res.sendStatus(500)
+})
+app.get('/',function (req,res) {
+	console.log("[%s] unknown params: %j, body: %j",req.originalUrl,req.params,req.body)
+	res.sendStatus(500)
 })
 
 app.listen(process.env.PORT || 3000,function(){
