32c4ceedad96fb14e89c594edd46ba71f000f69e
Rokke <ragnartalgoe@gmail.com>
2019-07-12 08:42:11 +0200

    .

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.get('/fights/:tpss/:date',function (req,res) {
     res.json(cjson)
   }else res.sendStatus(500)
 })
+app.get('/fights',function (req,res) {
+	console.log("[%s] TPSS fights feil: %j, %j",req.originalUrl,req.params,req.body)
+	let filename='/var/opt/tkd_json/'+req.params.tpss+'_'+req.params.date+'.json'
+  let cont=fs.readFileSync(filename)
+  if(cont){
+    let cjson=JSON.parse(cont)
+    console.log(req.originalUrl,cjson.fights.length,cjson.date)
+    res.json(cjson)
+  }else res.sendStatus(500)
+})
 app.put('/fights',function (req,res) {
 	console.log("[%s] TPSS fights put: %j, %j",req.originalUrl,req.params,req.body)
 	let filename='/var/opt/tkd_json/'+req.body.tpss+'_'+req.body.date+'.json'
