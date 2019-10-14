f884afc750c58ca5730a017b63fe42157c03ed14
Rokke <ragnartalgoe@gmail.com>
2019-10-14 22:07:48 +0200

    updated extensions and rotation

--- winston.js
+++ winston.js
@@ -18,8 +18,7 @@ var logger=createLogger({
 			filename: `${LOG_PATH}logs/tkdjson_%DATE%.log`,
 			datePattern: 'YYYY-MM-DD',
 			zippedArchive: true,
-			maxSize: '4m',
-			maxFiles: '14d'
+			maxFiles: '9d'
 		}),
     new transports.File({
       level: 'error', filename: `${LOG_PATH}logs/err_tkdjson.log`, handleExceptions: true, json: true, maxsize: 542880, colorize: false
